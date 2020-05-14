798319adca9236fc5e4c27f62cbcc0e8deecd555
root <root@DESKTOP-NGTHS2N.localdomain>
2020-05-14 18:45:57 +0800

    up

--- 3-etcd-service-discovery/register/register.go
+++ 3-etcd-service-discovery/register/register.go
@@ -61,7 +61,6 @@ func (s *ServiceRegister) putKeyWithLease(lease int64) error {
 		return err
 	}
 	s.leaseID = resp.ID
-	log.Println(s.leaseID)
 	s.keepAliveChan = leaseRespChan
 	log.Printf("Put key:%s  val:%s  success!", s.key, s.val)
 	return nil
